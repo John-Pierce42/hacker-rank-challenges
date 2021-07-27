01c2e59f9ab68084ee0ab541bb2a5c9949cf2205
John Pierce <pierce.johndaniel@gmail.com>
2021-07-26 19:16:46 -0500

    feat: completed a couple challenges from hacker rank

--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Solution {
+
+public static void main(String[] args){
+    System.out.println("Hello, World.");
+    System.out.println("Hello, Java.");
+
+//    ===================================== challenge 2 =====================
+
+    Scanner scan = new Scanner(System.in);
+    int a = scan.nextInt();
+    int b = scan.nextInt();
+    int c = scan.nextInt();
+
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+}
+
 }
