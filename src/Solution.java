676e774b66688c706713f600efb276be28ecd303
John Pierce <pierce.johndaniel@gmail.com>
2021-08-18 19:23:33 -0500

    feat: completed challenge 14 on hacker rank

--- src/Solution.java
+++ src/Solution.java
@@ -314,9 +314,22 @@ public class Solution {
     }
 
     public static String getSmallestAndLargest(String s, int k) {
-        String smallest = "";
-        String largest = "";
 
+        String sequence = s.substring(0, k);
+
+        String smallest = sequence;
+        String largest = sequence;
+
+        for(int i = 1; i <= (s.length() - k); i++){
+            sequence = s.substring(i, (i + k));
+
+            if(sequence.compareTo(smallest) < 0){
+                smallest = sequence;
+            }
+            if(sequence.compareTo(largest) > 0){
+                largest = sequence;
+            }
+        }
 
         return smallest + "\n" + largest;
     }
