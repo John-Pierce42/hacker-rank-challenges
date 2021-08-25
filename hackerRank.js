f51f8d4f99a31f2a2d6bf9987896c9d262f6c6c0
John Pierce <pierce.johndaniel@gmail.com>
2021-08-25 18:35:17 -0500

    feat: completed first challenge on hacker rank  javaScript

--- hackerRank.js
+++ hackerRank.js
@@ -1,2 +1,25 @@
+'use strict';
 
+/**
+ *   A line of code that prints "Hello, World!" on a new line is provided in the editor.
+ *   Write a second line of code that prints the contents of 'parameterVariable' on a new line.
+ *
+ *	Parameter:
+ *   parameterVariable - A string of text.
+ **/
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+    console.log(parameterVariable);
+}
+
+// function main() {
+//     const parameterVariable = readLine();
+//
+//     greeting(parameterVariable);
+// }
+
+greeting("My name is John I'm here to Explore the awesome world of new Technologies!");
 
