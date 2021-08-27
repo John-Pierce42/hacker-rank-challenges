dc24f67c4d9b928fe0562714fb10776aa6a821d3
John Pierce <pierce.johndaniel@gmail.com>
2021-08-27 18:15:57 -0500

    feat: completed 3rd challenge js hacker rank

--- hackerRank.js
+++ hackerRank.js
@@ -65,3 +65,38 @@ function main() {
 
     performOperation(secondInteger, secondDecimal, secondString);
 }
+
+//======================================================== 3rd challenge ==========================
+
+/**
+ *   Calculate the area of a rectangle.
+ *
+ *   length: The length of the rectangle.
+ *   width: The width of the rectangle.
+ *
+ *	Return a number denoting the rectangle's area.
+ **/
+function getArea(length, width) {
+    // let area;
+    // Write your code here
+
+    return length * width;
+}
+
+/**
+ *   Calculate the perimeter of a rectangle.
+ *
+ *	length: The length of the rectangle.
+ *   width: The width of the rectangle.
+ *
+ *	Return a number denoting the perimeter of a rectangle.
+ **/
+function getPerimeter(length, width) {
+    // let perimeter;
+    // Write your code here
+
+    return 2 * (length + width);
+}
+
+console.log(getPerimeter(2,2));
+console.log(getArea(2,2));
