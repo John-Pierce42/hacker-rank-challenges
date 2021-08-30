b683efb6bcfd48d5c16f87f674300cf30cbb0727
John Pierce <pierce.johndaniel@gmail.com>
2021-08-29 20:26:03 -0500

    feat: completed 5th challenge on hacker rank js

--- hackerRank.js
+++ hackerRank.js
@@ -140,3 +140,19 @@ function vowelsAndConsonants(s){
 }
 
 vowelsAndConsonants("javascript");
+
+// ======================================= 5th challenge =================================
+
+function factorial(n){
+    if(n < 0){
+        return -1;
+    } else if(n === 0){
+        return 1;
+    } else {
+        return (n * (factorial(n -1)));
+    }
+
+}
+
+console.log(factorial(4));
+
