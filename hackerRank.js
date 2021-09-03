f697c8afa13f4877cf757a69bc19d42eb9535e16
John Pierce <pierce.johndaniel@gmail.com>
2021-09-02 21:54:37 -0500

    feat: completed 9th challenge js hacker rank

--- hackerRank.js
+++ hackerRank.js
@@ -269,3 +269,15 @@ class Rectangle {
         this.h = h;
     }
 }
+
+Rectangle.prototype.area = function() {
+    return(this.w * this.h)
+}
+
+class Square extends Rectangle {
+    constructor(s) {
+        super(s);
+        this.h = s;
+        this.w = s;
+    }
+}
