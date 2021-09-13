bff2f67dd83ae32806f9eeb990e997ed4720ca34
John Pierce <pierce.johndaniel@gmail.com>
2021-09-12 23:58:51 -0500

    feat: completed challenge 14th on hacker rank

--- hackerRank.js
+++ hackerRank.js
@@ -387,3 +387,22 @@ function main() {
 }
 
 console.log(main());
+
+function getSecondLargest(nums) {
+    let largest = nums[0];
+    let secondLargest = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > largest) {
+            secondLargest = largest;
+            largest = nums[i];
+            continue;
+        }
+
+        if ((nums[i] > secondLargest) && (nums[i] < largest)) {
+            secondLargest = nums[i];
+        }
+    }
+
+    return secondLargest;
+}
