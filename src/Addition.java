8d3720f4816fa72fbeb0827a19ac6638688345a3
John Pierce <pierce.johndaniel@gmail.com>
2021-09-30 07:21:33 -0500

    feat:created a method for adding numbers

--- src/Addition.java
+++ src/Addition.java
@@ -1,5 +1,11 @@
 public class Addition {
 
+    int num1 = 5;
+    int num2 = 5;
+    public void adding(){
+        int added = num1 + num2;
+    }
+
 //    creating a new object =============
     public static void main(String[] args){
         Addition add = new Addition();
