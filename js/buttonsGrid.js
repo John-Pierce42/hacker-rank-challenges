83087cfeaae4780765f95158dc0ad4547ef15f4f
John Pierce <pierce.johndaniel@gmail.com>
2021-10-06 18:06:45 -0500

    feat: using classes and meathods

--- js/buttonsGrid.js
+++ js/buttonsGrid.js
@@ -23,4 +23,34 @@ btn5.addEventListener("click",()=> {
     for (let i = 0; i < btn_ids.length; i++) {
         document.getElementById("btn" + btn_ids[i]).innerHTML = btn_values[i];
 
-}}
+    };
+
+class Rotator {
+    constructor(values) {
+        this.values = values;
+    }
+
+    rotation() {
+        this.values.unshift(this.values.pop());
+    }
+
+    round() {
+        this.rotation();
+        this.render();
+    }
+
+    render() {
+        const [btn1, btn2, btn3, btn6, btn9, btn8, btn7, btn4] = this.values;
+        const hashTable = {btn1, btn2, btn3, btn6, btn9, btn8, btn7, btn4};
+
+        for (const key in hashTable) {
+            document.getElementById(key).innerHTML = hashTable[key];
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const rotator = new Rotator([1, 2, 3, 6, 9, 8, 7, 4]);
+    rotator.render();
+    document.getElementById('btn5').addEventListener('click', () => { rotator.round(); });
+});}
